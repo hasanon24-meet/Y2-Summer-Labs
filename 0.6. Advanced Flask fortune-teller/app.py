af963b73148d9d6a0e00fbe76f94bcd7d36ecf54
hasanon24-meet <onallahhasan@gmail.com>
2024-07-17 21:34:22 +0300

    i finished

--- 0.6. Advanced Flask fortune-teller/app.py
+++ 0.6. Advanced Flask fortune-teller/app.py
@@ -1,16 +1,26 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,redirect,url_for
 import random
-fortunelist=["youll get accepted to the MIT","Lill will win the worldcup","SARI KHAMIS will get beat up by hasan","Amir will win watan in a fist fight",]
 app = Flask(__name__,template_folder='templates')
 
-@app.route('/home')
-def home ():
-	return (render_template('lill.html'))
+fortunelist=["youll get accepted to the MIT","Lill will win the worldcup","SARI KHAMIS will get beat up by hasan","Amir will win watan in a fist fight","Shalev moves to Kazakhstan","lill will become cs lead","hasan stays th best in the world","hasan will do his bonus","Ibrahem becomes the president of the USA","SARI KHAMIS will become a y1 lead","meet wil add another meet value"]
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+	if request.method == 'GET':
+	    return render_template('lill.html')
+	else:
+	    birth = request.form['birth']	   
+	    return redirect(url_for('fortune',
+	        Birthmonth = birth))
+	return render_template('lill.html')
 
-@app.route('/fortune')
-def fortune():
-	rand=random.choice(fortunelist)
-	return(render_template('fortune.html',rand=rand))
+@app.route('/fortune/<Birthmonth>')
+def fortune(Birthmonth):
+
+	numbers=len(Birthmonth)
+	if numbers<10:
+		return(render_template('fortune.html',fortunemsg=fortunelist[len(Birthmonth)]))
+	else:
+		return render_template('finish.html')
 
 
 if __name__ == '__main__':   
