3e5bc958c37a0a82f4214af8a465199dd84b7639
hasanon24-meet <onallahhasan@gmail.com>
2024-07-25 15:27:21 +0300

    asf

--- watch service project/app.py
+++ watch service project/app.py
@@ -35,7 +35,7 @@ def signup():
         return redirect(url_for('choosen'))
 
       else:
-        return render_template('welcome.html')
+        return redirect(url_for('fixer'))
   
     except Exception as e:
       error = "Authentication failed"
@@ -49,11 +49,9 @@ def signin():
   if request.method == 'POST':
     email = request.form['email']
     password = request.form['password']
-    consumer = request.form['consumer']
-
     try:
       login_session['user'] = auth.sign_in_with_email_and_password(email, password)
-      return render_template('signin.html')
+      return redirect(url_for('proposal'))
     except:
       error = "Authentication failed"
       print(error)
@@ -67,13 +65,77 @@ def choosen():
     updatedinfo={"engine":sengine,"kind":ref}
     UID=login_session['user']['localId']
     db.child("Users").child(UID).update(updatedinfo)
-    return redirect(url_for('date'))
+    customerinfo=db.child()
+    return redirect(url_for('proposal'))
   return render_template('choosen.html')
 
+@app.route('/fixer', methods=['GET','POST'])
+def fixer():
+  consumers=db.child('Users').get().val()
+  return render_template("fixer.html", consumers=consumers)
+
+@app.route('/offer/<ida>', methods=['GET','POST'])
+def offer(ida):
+  print(ida)
+  if request.method=="POST":
+    offer = request.form['offer1']
+    updated2={"offer":offer}
+    UID=login_session['user']['localId']
+    db.child("Users").child(ida).update(updated2)
+    return render_template('offer.html', i=ida)
+  else:
+    return render_template('offer.html', i=ida)
+
+@app.route('/proposal', methods=['GET','POST'])
+def proposal():
+  UID=login_session['user']['localId']
+  customer=db.child("Users").child(UID).get().val()
+
+  if 'offer' in customer.keys():
+    prop=customer['offer']
+    
+    return render_template('date.html', p=prop)
+  return render_template('date.html')
 
 
-@app.route('/date', methods=['GET','POST'])
-def date():
+@app.route('/decision', methods=['GET','POST'])
+def decision():
+  if request.method == 'POST':
+    ans=request.form['opt']
+    if ans=='reject':
+      UID=login_session['user']['localId']
+      db.child("Users").child(UID).child('offer').remove()
+      return render_template('reject.html')
+
+    else:
+      UID=login_session['user']['localId']
+      db.child("Users").child(UID).child('offer').remove()
+      return render_template('accept.html')
+  else:
+    return render_template('date.html')
+
+
+
+@app.route('/signout', methods=['GET', 'POST'])
+def signout():
+  login_session['user']=None
+  auth.current_user=None
+
+  return redirect(url_for('signin'))
+
+
+
+
+
+
+
+
+
+
+    
+    
+ 
+
   return render_template("date.html")
 if __name__ == '__main__':   
     app.run(debug=True)   
